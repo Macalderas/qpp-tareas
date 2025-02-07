100055783e459c2ff4e6c783489a6ed11bb07870
macalderas <macalderas@scl.edu.gt>
2025-02-07 11:59:44 -0600

    pruebra

--- componet/cuerpo/cuerpo.js
+++ componet/cuerpo/cuerpo.js
@@ -1,7 +1,26 @@
-function cuerpo() {
-    let cuerpo = document.createElement("section")
-    tarea.className="cuerpo"
-    return cuerpo
-  }
+
+function cuerpo( descripcion) {
+  let div = document.createElement('div');
+  div.className = "cuerpo";
+
+  let boton = document.createElement('div');
+  boton.className = "Boton";
+  boton.addEventListener("click", function () {
+    boton.classList.toggle("activo"); 
+  });
+
+  div.appendChild(boton);
+
+   let content = document.createElement('div');
+
   
-  export { cuerpo};
+  let p = document.createElement('p');
+  p.textContent = descripcion;
+  content.appendChild(p);
+
+  div.appendChild(content);
+
+  return div;
+}
+
+export { cuerpo };
--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import { crearHeader } from "./componet/header /header.js"
-import { formulario } from "./componet/formulario/formulario.js"
 import { cuerpo } from "./componet/cuerpo/cuerpo.js"
+import { formulario } from "./componet/formulario/formulario.js"
 
 let DOM =document.getElementById("root")
 
-DOM.appendChild(crearHeader())
-DOM.appendChild(tarea())
-DOM.appendChild(formulario())
+DOM.appendChild(crearHeader());
+DOM.appendChild(cuerpo("Create icons for a dashboard")); 
+DOM.appendChild(cuerpo("Prepare a design presentation.")); 
+DOM.appendChild(cuerpo("Stretch for 15 minutes"));
+DOM.appendChild(cuerpo("Plan your meal")); 
+DOM.appendChild(cuerpo("Review daily goals before sleeping. Add some new if time permits")); 
+DOM.appendChild(cuerpo("Water indoor plants")); 
+
+DOM.appendChild(formulario());
